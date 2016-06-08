b9b269d856ec679e5845a23d57db147c4fa4be41
minh <pesrobot@gmail.com>
2016-06-08 18:01:08 +0300

    completing

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+var app = angular.module('myApp', [])
+.controller('myCtrl', function($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
 	$scope.appDetails = {
 		title : "BooKart",
 		tagline : "We have collection of 1 Million books"
 	};
-});
-
-var BookListCtrl = function($scope){
+})
+.controller('BookListCtrl', function($scope) {
 	$scope.books = [
-		{
-			imgUrl: "1.png", 
-			name: "The first book",
-			price: 205,
-			rating: 3,
-			binding: "Paperback",
-			publisher: "Random publisher",
-			releaseDate: "12-09-2013",
-			details: "Rob in his thirties"
-		},
-		{
-			imgUrl: "2.png", 
-			name: "Gerome the great fighter",
-			price: 102,
-			rating: 4,
-			binding: "Paperback",
-			publisher: "Scholastic",
-			releaseDate: "01-07-2015",
-			details: "Gerome fought his way"
-		},
-		{
-			imgUrl: "3.png", 
-			name: "Life",
-			price: 301,
-			rating: 5,
-			binding: "Paperback",
-			publisher: "Hatchette",
-			releaseDate: "01-01-2015",
-			details: "Seeing the opportunities in life"
-		}
-	]
-}
+			{
+				imgUrl: "", 
+				name: "The first book",
+				price: 205,
+				rating: 3,
+				binding: "Paperback",
+				publisher: "Random publisher",
+				releaseDate: "12-09-2013",
+				details: "Rob in his thirties"
+			},
+			{
+				imgUrl: "", 
+				name: "Gerome the great fighter",
+				price: 102,
+				rating: 4,
+				binding: "Paperback",
+				publisher: "Scholastic",
+				releaseDate: "01-07-2015",
+				details: "Gerome fought his way"
+			},
+			{
+				imgUrl: "", 
+				name: "Life",
+				price: 301,
+				rating: 5,
+				binding: "Paperback",
+				publisher: "Hatchette",
+				releaseDate: "01-01-2015",
+				details: "Seeing the opportunities in life"
+			}
+	];
+	 $scope.addToCart = function(book){
+		 console.log("added to cart", book);
+	 }
+});		
